e1259f331f79b294b1971837fc3215a2ed6ec149
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-01-11 00:33:15 +0300

    Program to get the last digit of an integer then check it against conditions set

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import random
+
+number = random.randint(-10000, 10000)
+
+# 1. Accessing the string representation of a number.
+n_str = repr(number)
+
+# 2. Accessing the last string of digit string.
+last_dt_str = n_str[-1]
+
+# 3. Converting the last string into an integer.
+last_dt = int(last_dt_str)
+
+if last_dt > 5:
+    print(f"Last digit of {number} is {last_dt} and is greater than 5")
+elif last_dt == 0:
+    print(f"Last digit of {number} is {last_dt} and is 0")
+else:
+    print(f"Last digit of {number} is {last_dt} and is less than 6 and not 0")
