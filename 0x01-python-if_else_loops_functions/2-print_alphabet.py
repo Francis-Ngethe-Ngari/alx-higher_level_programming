265a62c84c1063caaed16680a8ccd52d9c285870
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-03-04 17:21:27 +0300

    Declared program that prints ASCII alphabets in lowercase

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.
+# Program to print Ascii alphabets, in Lowercase.
+def LowerCase():
+
+    for letters in range(97, 123):
+
+        print(chr(letters), end=" ")
+
+
+LowerCase()
