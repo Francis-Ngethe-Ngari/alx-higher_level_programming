eae9cfbeb6071f7ae2b7b1b3fcccc694e3ff052c
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-03-12 15:09:10 +0300

    Added statement FULL to show columns statement to get full description of table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- Program to describe table: first_table from selected db.
 -- NB: DESCRIBE or EXPLAIN statements should not be used.
 
-SHOW COLUMNS FROM first_table;
+SHOW FULL COLUMNS FROM first_table;
