16aecc1bcca3d6f4c3925fc4164d29f102296886
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-03-12 20:19:57 +0300

    Program exempts NULL values in name column

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,7 @@
- -- Program lists records of two columns in a respective order.
--- Score column output is given in descending order.
+-- Program lists all records in column score and name
+-- except records whereby name is NULL(No value was added).
 
 SELECT score, name
 FROM second_table
+WHERE name IS NOT NULL
 ORDER BY score DESC;
