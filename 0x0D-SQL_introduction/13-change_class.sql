61b484c188ebfeb7082b2cd8619041bd6ad75bf4
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-03-12 18:36:13 +0300

    Declared delete statement in script to remove specific records that did not meet a criteria

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,7 @@
+-- Program removes records which are related to having a score less
+-- than and equal to 5.
+-- Delete statement is used to remove records from a table.
+-- Action is irreversible.
+
+DELETE FROM second_table
+WHERE score <= 5;
