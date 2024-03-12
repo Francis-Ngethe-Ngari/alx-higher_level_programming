13fc3d80abbd0950567c7ab4ad1146ac8395dceb
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-03-12 12:29:02 +0300

    Program creates new db or else does not throw error if db exists

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- Program aims to create a new database if it doesn.t exist.
+-- If it exists it should not through an error.
+-- SELECT and SHOW statements are not allowed.
+
+CREATE database IF NOT EXISTS school_db;
