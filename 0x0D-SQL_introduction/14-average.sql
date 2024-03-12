2b247e6b6915d0fbacebeb15fcde79b280de28a1
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-03-12 18:46:55 +0300

    Program computes average using Aggrigate function AVG

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,6 @@
+-- Programs computes average score and displays output
+-- under an alias column named average.
+-- Average is an aggregate function.
+
+SELECT AVG(score) AS average
+FROM second_table;
