99d272611c119224e73d40a5ac19ea08cee55e71
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-03-12 20:36:36 +0300

    Changed program to use SQL schema to retrieve description of table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,7 @@
 -- Program to describe table: first_table from selected db.
 -- NB: DESCRIBE or EXPLAIN statements should not be used.
 
-SHOW FULL COLUMNS FROM first_table;
+-- SHOW FULL COLUMNS FROM first_table;
+SELECT *
+FROM information_schema.columns 
+WHERE table_name = 'first_table';
