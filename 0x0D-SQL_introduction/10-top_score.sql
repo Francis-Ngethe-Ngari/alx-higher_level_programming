abbbe84be67d8ad84ad34c3529ab834809bdf4f9
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-03-12 17:00:49 +0300

    Program sorts queried data in descending order

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,7 @@
+-- Program list score and name records in second_table
+-- in descending order (Top first).
+-- ORDER BY CLAUSE is used to sort the given records.
+
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
