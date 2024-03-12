18e3d0e345fe48c1134467da415b4ff9d97f28f9
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-03-12 16:07:06 +0300

    Program filters data based on a given criteria i.e id = 89

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,7 @@
+-- Program check no of records in table: first_table in which
+-- id column = 89. WHERE statement is used to filter results to
+-- specific requirements.
+
+SELECT *
+	FROM first_table
+	WHERE id = 89;
