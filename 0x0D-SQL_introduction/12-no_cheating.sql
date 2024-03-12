ba32c75b0fc4ae22fc08d2afb4e1304b694f4e5f
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-03-12 17:31:28 +0300

    Slight change to chosen criteria: Score updated should be for Bob

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,8 +1,8 @@
--- Program updates Johns score to 10
+-- Program updates Bob's score to 10
 -- UPDATE statement modifies data in tables
 -- within database chosen.
 -- WHERE CLAUSE is used to filter data.
 
 UPDATE second_table
 SET score = 10
-WHERE name = "John";
+WHERE name = "Bob";
