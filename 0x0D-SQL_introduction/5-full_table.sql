740e44a067303adc96955006ad2bea0b45f98e4a
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-03-12 14:30:28 +0300

    reverted back to using SHOW command to get description of a table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,7 +1,4 @@
 -- Program to describe table: first_table from selected db.
 -- NB: DESCRIBE or EXPLAIN statements should not be used.
 
--- SHOW COLUMNS FROM first_table;
-SELECT *
-	FROM INFORMATION_SCHEMA.COLUMNS
-	WHERE table_name = 'first_table';
+SHOW COLUMNS FROM first_table;
