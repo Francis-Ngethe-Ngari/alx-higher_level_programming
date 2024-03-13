b33a112c300ef85ec924b392db15ad338d2d5889
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-03-13 19:24:37 +0300

    Updating program to convert encoding to utf8mb4 and collate to utf8mb4_unicode_ci

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,14 +1,14 @@
--- Programs converts selected the following entities to utf8 encoding from utf8mb4:
+-- Programs converts selected the following entities to utf8mb4:
 -- Database: hbtn_0c_0 
 -- Table: first_table
 -- column: name
 
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8 COLLATE = utf8_general_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 USE hbtn_0c_0;
 
 ALTER TABLE first_table ROW_FORMAT=DYNAMIC;
 
-ALTER TABLE first_table CHARACTER SET = utf8 COLLATE = utf8_general_ci;
+ALTER TABLE first_table CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
-ALTER TABLE first_table CHANGE name name VARCHAR (256) CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE first_table CHANGE name name VARCHAR (256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
