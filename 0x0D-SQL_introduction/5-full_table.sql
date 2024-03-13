a4401ff82e7c7bafa28441c3ef1eb02171758ab5
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-03-13 18:58:53 +0300

    Updated command to display statements used to create the specified table and other table related data

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,7 +1,6 @@
 -- Program to describe table: first_table from selected db.
 -- NB: DESCRIBE or EXPLAIN statements should not be used.
+-- Command used below will display statements used to create
+-- table specified. 
 
--- SHOW FULL COLUMNS FROM first_table;
-SELECT *
-FROM information_schema.columns 
-WHERE table_name = 'first_table';
+SHOW CREATE TABLE first_table;
