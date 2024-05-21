ece9c332b3710f3ddc9eacf9b6ff382d59a4a859
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-05-21 22:34:53 +0300

    re-wrote code to get last digit using absolute value to deal with negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,18 +4,13 @@ import random
 
 number = random.randint(-10000, 10000)
 
-# 1. Accessing the string representation of a number.
-n_str = repr(number)
+# Get last digit of a number
+last_dt = abs(number) % 10
 
-# 2. Accessing the last string of digit string.
-last_dt_str = n_str[-1]
-
-# 3. Converting the last string into an integer.
-last_dt = int(last_dt_str)
-
-if ((number > 5) and (last_dt > 5)):
+if last_dt > 5:
     print(f"Last digit of {number} is {last_dt} and is greater than 5")
-elif ((number > 0) and (last_dt == 0)):
-    print(f"Last digit of {number} is {last_dt} and is 0")
+elif last_dt == 0:
+    if number > 0:
+        print(f"Last digit of {number} is {last_dt} and is 0")
 elif ((last_dt < 6) and (last_dt > 0)):
     print(f"Last digit of {number} is {last_dt} and is less than 6 and not 0")
