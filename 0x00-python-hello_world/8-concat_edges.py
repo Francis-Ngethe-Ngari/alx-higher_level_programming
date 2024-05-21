70798c734a83a3e2d30ad168f03ea6f43eec8ca1
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-05-21 18:53:22 +0300

    Implemented indexing to print section of the sentence shown in the example

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = (lambda x: x[2] + x[1] + x[0] + x[3:])(str.partition(" ")[::-1])
+str = str[39:79]
 print(str)
