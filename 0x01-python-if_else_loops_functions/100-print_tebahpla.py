4f15888064a6ba9960e206c827a91b9fae830e21
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-05-23 15:56:43 +0300

    changed the loop variable from char to i to meet number of chars requirement

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,8 +2,8 @@
 
 def alternate_char():
 
-    for char in range(122, 96, -1):
-        print(chr(char) if char % 2 == 0 else chr(char - 32), end='')
+    for i in range(122, 96, -1):
+        print("{}".format(chr(i)) if i % 2 == 0 else chr(i - 32), end='')
 
 
 alternate_char()
